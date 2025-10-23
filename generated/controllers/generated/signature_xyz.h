8a6f689fbd672fc5a5a728d5c3e51f23ed0880ba
rusefillc <rusefillc@gmail.com>
2025-10-23 07:39:13 -0400

    hello canBridge

--- generated/controllers/generated/signature_xyz.h
+++
@@ -1,6 +0,0 @@
-//
-// CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 3453165914
-#define TS_SIGNATURE "rusEFI main.2025.10.23.xyz.3453165914"
