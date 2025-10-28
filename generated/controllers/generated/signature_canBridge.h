50bce95f248d0d6eaa9e6e80fb3218fafe12e46f
GitHub git update Action <action@github.com>
2025-10-28 00:42:21 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_canBridge.h
+++ generated/controllers/generated/signature_canBridge.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2889503397
-#define TS_SIGNATURE "rusEFI main.2025.10.27.canBridge.2889503397"
+#define SIGNATURE_HASH 398662595
+#define TS_SIGNATURE "rusEFI main.2025.10.28.canBridge.398662595"
