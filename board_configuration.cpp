cfb5c3f82b4fde5a212a051e84dfde4ac084c7a2
rusefillc <rusefillc@gmail.com>
2025-11-05 22:18:37 -0500

    luaScript

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "board_overrides.h"
 #include "hellen_meta.h"
+#include "lua_lib.h"
 
 Gpio getCommsLedPin() {
 	return Gpio::Unassigned;
@@ -54,6 +55,18 @@ static void customBoardDefaultConfiguration() {
 
 	// Battery sense on PA0
 //	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
+
+
+	strncpy(config->luaScript, GET_BIT_RANGE_LSB TWO_BYTES_LSB PRINT_ARRAY SET_TWO_BYTES_LSB R"(
+
+enableCanRxWorkaround()
+global_can_data = {}
+
+
+function onTick()
+end
+
+    )", efi::size(config->luaScript));
 }
 
 void setup_custom_board_overrides() {
