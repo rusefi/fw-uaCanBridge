040bd12fcdb5d8fee0d77888ccd3d96edc331ba7
rusefillc <rusefillc@gmail.com>
2025-11-23 20:16:13 -0500

    helping simulator

--- firmware/firmware.mk
+++ firmware/firmware.mk
@@ -1,6 +1,6 @@
-#ifneq ($(PROJECT_CPU),simulator)
+ifneq ($(PROJECT_CPU),simulator)
 BOARDCPPSRC += \
     $(BOARD_DIR)/firmware/hardware/board_hw_test.cpp \
     $(BOARD_DIR)/board_configuration.cpp \
 
-#endif
+endif
