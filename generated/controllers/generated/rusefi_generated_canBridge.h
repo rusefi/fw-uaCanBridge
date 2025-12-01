77d3ee902792ef902d32150465fbfa5d31310e3a
GitHub git update Action <action@github.com>
2025-12-01 00:48:39 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_canBridge.h
+++ generated/controllers/generated/rusefi_generated_canBridge.h
@@ -2129,7 +2129,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.11.30.canBridge.3218610234"
+#define TS_SIGNATURE "rusEFI main.2025.12.01.canBridge.3218610234"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
