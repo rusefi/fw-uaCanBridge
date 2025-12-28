e0e6a009aa7f89f9553fdca5c7757e911f1d40e9
GitHub git update Action <action@github.com>
2025-12-28 18:05:14 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_canBridge.h
+++ generated/controllers/generated/rusefi_generated_canBridge.h
@@ -1544,7 +1544,7 @@
 #define show_tcu_gauges false
 #define show_vvl_control false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2516506349
+#define SIGNATURE_HASH 2260001626
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1918,7 +1918,7 @@
 #define ts_show_can2 true
 #define ts_show_can_bitrate true
 #define ts_show_can_pins false
-#define ts_show_can_verbose false
+#define ts_show_can_verbose true
 #define ts_show_can_wbo true
 #define ts_show_can_wbo_type true
 #define ts_show_can_weird false
@@ -2135,7 +2135,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.12.28.canBridge.2516506349"
+#define TS_SIGNATURE "rusEFI main.2025.12.28.canBridge.2260001626"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_canBridge.h
+++ generated/controllers/generated/signature_canBridge.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2516506349
-#define TS_SIGNATURE "rusEFI main.2025.12.28.canBridge.2516506349"
+#define SIGNATURE_HASH 2260001626
+#define TS_SIGNATURE "rusEFI main.2025.12.28.canBridge.2260001626"
