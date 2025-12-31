eeedde3867f06c337aba7000ce0e2b45e65f854d
GitHub git update Action <action@github.com>
2025-12-31 05:27:19 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_canBridge.h
+++ generated/controllers/generated/rusefi_generated_canBridge.h
@@ -1544,7 +1544,7 @@
 #define show_tcu_gauges false
 #define show_vvl_control false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2260001626
+#define SIGNATURE_HASH 4023471329
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1921,7 +1921,7 @@
 #define ts_show_can_verbose true
 #define ts_show_can_wbo true
 #define ts_show_can_wbo_type true
-#define ts_show_can_weird false
+#define ts_show_can_weird true
 #define ts_show_charge_estimation true
 #define ts_show_check_engine false
 #define ts_show_clt true
@@ -2135,7 +2135,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.12.31.canBridge.2260001626"
+#define TS_SIGNATURE "rusEFI main.2025.12.31.canBridge.4023471329"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
