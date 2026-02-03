368e59d76d9f368a009cdf65799db1c75f05d85f
Andrey Gusakov <dron0gus@gmail.com>
2026-02-03 11:34:34 +0300

    compile_firmware.sh: accept arguments

--- compile_firmware.sh
+++ compile_firmware.sh
@@ -7,4 +7,4 @@ echo "META_OUTPUT_ROOT_FOLDER=$META_OUTPUT_ROOT_FOLDER"
 
 export AUTOMATION_REF=`git branch --show-current`
 cd ext/rusefi/firmware/
-bash bin/compile.sh ../../../meta-info.env
+bash bin/compile.sh ../../../meta-info.env $*
