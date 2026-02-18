d922ff4ba693cf8d7d92bc620ee4fe72aad70443
GitHub git update Action <action@github.com>
2026-02-18 00:51:12 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_canBridge.h
+++ generated/controllers/generated/rusefi_generated_canBridge.h
@@ -1555,7 +1555,7 @@
 #define show_tcu_gauges false
 #define show_vvl_control false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 404722345
+#define SIGNATURE_HASH 3036338142
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2153,7 +2153,7 @@
 #define ts_show_wbo_can_menu true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2026.02.17.canBridge.404722345"
+#define TS_SIGNATURE "rusEFI main.2026.02.18.canBridge.3036338142"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_canBridge.h
+++ generated/controllers/generated/signature_canBridge.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 404722345
-#define TS_SIGNATURE "rusEFI main.2026.02.17.canBridge.404722345"
+#define SIGNATURE_HASH 3036338142
+#define TS_SIGNATURE "rusEFI main.2026.02.18.canBridge.3036338142"
