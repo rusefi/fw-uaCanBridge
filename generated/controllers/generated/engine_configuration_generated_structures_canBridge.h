5c18f94f9d3aca710460d61f065a122776904748
GitHub git update Action <action@github.com>
2026-04-20 01:02:44 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/engine_configuration_generated_structures_canBridge.h
+++ generated/controllers/generated/engine_configuration_generated_structures_canBridge.h
@@ -6279,6 +6279,16 @@ struct blend_table_s {
 };
 static_assert(sizeof(blend_table_s) == 188);
 
+// start of KnockGain
+struct KnockGain {
+	/**
+	 * units: dB
+	 * offset 0
+	 */
+	int8_t table[6][6] = {};
+};
+static_assert(sizeof(KnockGain) == 36);
+
 // start of persistent_config_s
 struct persistent_config_s {
 	/**
@@ -7813,8 +7823,21 @@ struct persistent_config_s {
 	/**
 	offset 63900 bit 31 */
 	bool unusedBit_286_31 : 1 {};
+	/**
+	 * offset 63904
+	 */
+	scaled_channel<uint8_t, 1, 10> knockGainLoadBins[6] = {};
+	/**
+	 * units: RPM
+	 * offset 63910
+	 */
+	scaled_channel<uint8_t, 1, 100> knockGainRpmBins[6] = {};
+	/**
+	 * offset 63916
+	 */
+	KnockGain knockGains[MAX_CYLINDER_COUNT] = {};
 };
-static_assert(sizeof(persistent_config_s) == 63904);
+static_assert(sizeof(persistent_config_s) == 64348);
 
 // end
 // this section was generated automatically by rusEFI tool config_definition-all.jar based on (unknown script) integration/rusefi_config.txt
--- generated/controllers/generated/signature_canBridge.h
+++ generated/controllers/generated/signature_canBridge.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3793179022
-#define TS_SIGNATURE "rusEFI main.2026.04.19.canBridge.3793179022"
+#define SIGNATURE_HASH 1506213753
+#define TS_SIGNATURE "rusEFI main.2026.04.20.canBridge.1506213753"
